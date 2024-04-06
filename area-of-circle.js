061ca0f84caf733b85ebe1b7d3113a5be52d74f0
Nicoly-Almeida <nicolyejady@gmail.com>
2024-04-06 14:04:14 -0300

    first commit

---
+++ area-of-circle.js
@@ -0,0 +1,13 @@
+const PI = 3.14
+let raio = 2
+
+let areaCirculo = PI * raio ** 2
+
+function areaCirc(raio) {
+    return PI * raio ** 2
+}
+
+console.log(areaCirculo)
+console.log(areaCirc(2))
+console.log(areaCirc(3))
+console.log(areaCirc(4))
---
+++ calc.js
@@ -0,0 +1,14 @@
+switch (operador){
+    case '+':
+        resultado = num1 + num2;
+        break;  
+    case '-':
+        resultado = num1 - num2;
+        break;
+    case '*':
+        resultado = num1 * num2;
+        break;
+    case '/':
+        resultado = num1 / num2;
+        break;
+}
